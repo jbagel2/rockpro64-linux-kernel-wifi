61a4e89d01b71737e911db1ead1d822227e9dc2d
Kamil Trzciński <ayufan@ayufan.eu>
2018-07-15 22:15:46 +0200

    ayufan: rockchip_wlan: revert changes
    Change-Id: I6aa0bbc24e2fdfb6da350ed167d59c4eea836c2a

--- drivers/net/wireless/rockchip_wlan/rtl8192du/core/rtw_security.c
+++ drivers/net/wireless/rockchip_wlan/rtl8192du/core/rtw_security.c
@@ -1501,9 +1501,8 @@ _func_enter_;
     for (j = 0 ; j < 8; j++) mic[j] = aes_out[j];
 
     /* Insert MIC into payload */
-    for (j = 0; j < 8; j++) {
+    for (j = 0; j < 8; j++)
     	pframe[payload_index+j] = mic[j];	//message[payload_index+j] = mic[j];
-	}
 
 	payload_index = hdrlen + 8;
 	for (i=0; i< num_blocks; i++)
@@ -1876,9 +1875,8 @@ _func_enter_;
     for (j = 0 ; j < 8; j++) mic[j] = aes_out[j];
 
     /* Insert MIC into payload */
-    for (j = 0; j < 8; j++) {
-		message[payload_index+j] = mic[j];
-	}
+    for (j = 0; j < 8; j++)
+    	message[payload_index+j] = mic[j];
 
 	payload_index = hdrlen + 8;
 	for (i=0; i< num_blocks; i++)
