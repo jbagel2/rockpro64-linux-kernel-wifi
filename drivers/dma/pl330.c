fce07c6e15ab5466e7ad75240d804902380d2a53
Kamil Trzciński <ayufan@ayufan.eu>
2018-09-29 18:34:16 +0200

    Revert "dmaengine: pl330: _loop_cyclic: fixup loopcnt is too large"
    This reverts commit 50e34163ae4c9a3067878dfe3c4e4444b6332bd3.

--- drivers/dma/pl330.c
+++ drivers/dma/pl330.c
@@ -1323,13 +1323,18 @@ static inline int _loop(struct pl330_dmac *pl330, unsigned dry_run, u8 buf[],
 	return off;
 }
 
-static int _period(struct pl330_dmac *pl330, unsigned int dry_run, u8 buf[],
-		   unsigned long bursts, const struct _xfer_spec *pxs, int ev)
+/* Returns bytes consumed */
+static inline int _loop_cyclic(struct pl330_dmac *pl330, unsigned dry_run,
+		u8 buf[], unsigned long bursts, const struct _xfer_spec *pxs, int ev)
 {
-	unsigned int lcnt1, ljmp1;
-	int cyc, off = 0;
+	int cyc, off;
+	unsigned lcnt0, lcnt1, ljmp0, ljmp1, ljmpfe;
 	struct _arg_LPEND lpend;
 	struct pl330_xfer *x = &pxs->desc->px;
+
+	off = 0;
+	ljmpfe = off;
+	lcnt0 = pxs->desc->num_periods;
 
 	if (bursts > 256) {
 		lcnt1 = 256;
@@ -1338,6 +1343,18 @@ static int _period(struct pl330_dmac *pl330, unsigned int dry_run, u8 buf[],
 		lcnt1 = bursts;
 		cyc = 1;
 	}
+
+	/* forever loop */
+	off += _emit_MOV(dry_run, &buf[off], SAR, x->src_addr);
+	off += _emit_MOV(dry_run, &buf[off], DAR, x->dst_addr);
+#ifdef CONFIG_ARCH_ROCKCHIP
+	if (!(pl330->quirks & PL330_QUIRK_BROKEN_NO_FLUSHP))
+		off += _emit_FLUSHP(dry_run, &buf[off],
+					pxs->desc->peri);
+#endif
+	/* loop0 */
+	off += _emit_LP(dry_run, &buf[off], 0,  lcnt0);
+	ljmp0 = off;
 
 	/* loop1 */
 	off += _emit_LP(dry_run, &buf[off], 1, lcnt1);
@@ -1387,53 +1404,11 @@ static int _period(struct pl330_dmac *pl330, unsigned int dry_run, u8 buf[],
 
 	off += _emit_SEV(dry_run, &buf[off], ev);
 
-	return off;
-}
-
-/* Returns bytes consumed */
-static inline int _loop_cyclic(struct pl330_dmac *pl330, unsigned int dry_run,
-		u8 buf[], unsigned long bursts, const struct _xfer_spec *pxs, int ev)
-{
-	int off, periods, residue, i;
-	unsigned int lcnt0, ljmp0, ljmpfe;
-	struct _arg_LPEND lpend;
-	struct pl330_xfer *x = &pxs->desc->px;
-
-	off = 0;
-	ljmpfe = off;
-	lcnt0 = pxs->desc->num_periods;
-	periods = 1;
-
-	while (lcnt0 > 256) {
-		periods++;
-		lcnt0 = pxs->desc->num_periods / periods;
-	}
-
-	residue = pxs->desc->num_periods % periods;
-
-	/* forever loop */
-	off += _emit_MOV(dry_run, &buf[off], SAR, x->src_addr);
-	off += _emit_MOV(dry_run, &buf[off], DAR, x->dst_addr);
-#ifdef CONFIG_ARCH_ROCKCHIP
-	if (!(pl330->quirks & PL330_QUIRK_BROKEN_NO_FLUSHP))
-		off += _emit_FLUSHP(dry_run, &buf[off],
-					pxs->desc->peri);
-#endif
-	/* loop0 */
-	off += _emit_LP(dry_run, &buf[off], 0,  lcnt0);
-	ljmp0 = off;
-
-	for (i = 0; i < periods; i++)
-		off += _period(pl330, dry_run, &buf[off], bursts, pxs, ev);
-
 	lpend.cond = ALWAYS;
 	lpend.forever = false;
 	lpend.loop = 0;
 	lpend.bjump = off - ljmp0;
 	off += _emit_LPEND(dry_run, &buf[off], &lpend);
-
-	for (i = 0; i < residue; i++)
-		off += _period(pl330, dry_run, &buf[off], bursts, pxs, ev);
 
 	lpend.cond = ALWAYS;
 	lpend.forever = true;
